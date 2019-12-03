4177f68b84d6816543a397077995ceddea451606
Jonathan Gonsowski <jondgons@ut.utm.edu>
2019-12-03 10:35:27 -0600

    DateEntry check attempt to check Amount number only

--- Financial Planner/MainWindow.xaml.cs
+++ Financial Planner/MainWindow.xaml.cs
@@ -26,28 +26,52 @@ namespace Financial_Planner
             InitializeComponent();
         }
 
+        // allows foreach() across a range of DateTimes
         public IEnumerable<DateTime> EachDay(DateTime from, DateTime thru)
         {
             for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
                 yield return day;
         }
 
+        // updates DataDisplay with range of information from StartDate to EndDate
         private void DisplayDateRange_Click(object sender, RoutedEventArgs e)
         {
+            DateTime StartDate = new DateTime();
+            DateTime EndDate = new DateTime();
+
             // wipes DataDisplay
             DataDisplay.Text = "";
 
-            // creates a DateTime from StartDatePicker
-            DateTime StartDate = DateTime.ParseExact(dateTextHelper(StartDatePicker.Text), "MMddyyyy", CultureInfo.InvariantCulture);
-            DateTime EndDate = DateTime.ParseExact(dateTextHelper(EndDatePicker.Text), "MMddyyyy", CultureInfo.InvariantCulture);
+            // creates a DateTime from StartDatePicker & checks if valid
+            if (StartDatePicker.Text != "")
+            {
+                StartDate = DateTime.ParseExact(dateTextHelper(StartDatePicker.Text), "MMddyyyy", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a start date.", "ERROR: Bad Date", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // creates a DateTime from EndDatePicker & checks if valid
+            if (EndDatePicker.Text != "")
+            {
+                EndDate = DateTime.ParseExact(dateTextHelper(EndDatePicker.Text), "MMddyyyy", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                MessageBox.Show("Please enter an end date.", "ERROR: Bad Date", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
 
             foreach (DateTime day in EachDay(StartDate, EndDate))
             {
                 // get data from database and add to DataDisplay
-                DataDisplay.Text += day.ToString() + "\n";
+                DataDisplay.Text += day.ToShortDateString() + "\n";
             }
         }
 
+        // converts date format to something readable by DateTime's ParseExact
         private string dateTextHelper(string dateText)
         {
             int slash = 0;
@@ -87,6 +111,36 @@ namespace Financial_Planner
             }
 
             return MM + dd + yyyy;
+        }
+
+        private void AddEntryButton_Click(object sender, RoutedEventArgs e)
+        {
+            // creates DateTime to read correct date from DateEntryPicker
+            DateTime EntryDate = new DateTime();
+            if (DateEntryPicker.Text != "")
+            {
+                EntryDate = DateTime.ParseExact(dateTextHelper(DateEntryPicker.Text), "MMddyyyy", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                MessageBox.Show("Please enter an entry date.", "ERROR: Bad Date", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        // TODO fix VerifyAmount
+        private bool nonNumberEntered = false;
+        private void VerifyAmount_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void VerifyAmount_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (nonNumberEntered)
+            {
+                e.Handled = true;
+            }
         }
 
         /*
